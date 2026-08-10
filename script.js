e9acd7f1d09a33034b4799ddbff94b68b8a4085f
sajjadkalhoro901-ops <sajjadkalhoro901@gmail.com>
2026-08-10 09:46:26 +0500

    Load final anime navigation cleanup

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /* SK Movies front-end loader. */
 (function(){
   function load(src){return new Promise((resolve,reject)=>{const s=document.createElement('script');s.src=src;s.onload=resolve;s.onerror=reject;document.head.appendChild(s)})}
-  load('site-core.js').then(()=>load('poster-fixes.js')).then(()=>load('anime-player.js')).then(()=>load('test-player.js')).then(()=>load('series-links.js')).catch(()=>{});
+  load('site-core.js').then(()=>load('poster-fixes.js')).then(()=>load('anime-player.js')).then(()=>load('test-player.js')).then(()=>load('series-links.js')).then(()=>load('final-fixes.js')).catch(()=>{});
 })();
